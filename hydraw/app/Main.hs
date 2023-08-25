8cc95a4ae906d7341e3117054ec36572f485ede5
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-08-26 00:44:24 +0200

    Step 1: HTML/JS only

--- hydraw/app/Main.hs
+++ hydraw/app/Main.hs
@@ -2,10 +2,7 @@ module Main where
 
 import Hydra.Prelude
 
-import Hydra.Cardano.Api (NetworkId (..), NetworkMagic (..))
-import Hydra.Network (Host, readHost)
-import Hydra.Painter (Pixel (..), paintPixel, withClient)
-import Network.HTTP.Types.Status (status200, status400, status404)
+import Network.HTTP.Types.Status (status200, status404)
 import Network.Wai (
   Application,
   pathInfo,
@@ -14,18 +11,10 @@ import Network.Wai (
   responseLBS,
  )
 import qualified Network.Wai.Handler.Warp as Warp
-import qualified Network.Wai.Handler.WebSockets as Wai
-import qualified Network.WebSockets as WS
-import Safe (readMay)
 
 main :: IO ()
 main = do
-  key <- requireEnv "HYDRAW_CARDANO_SIGNING_KEY"
-  host <- parseHost =<< requireEnv "HYDRA_API_HOST"
-  network <- parseNetwork =<< requireEnv "HYDRAW_NETWORK"
-  withClient host $ \cnx ->
-    Warp.runSettings settings $
-      Wai.websocketsOr WS.defaultConnectionOptions (websocketApp host) (httpApp network key cnx)
+  Warp.runSettings settings httpApp
  where
   port = 1337
 
@@ -39,48 +28,9 @@ main = do
             putStrLn $ "Listening on: tcp/" <> show port
         )
 
-  parseHost str =
-    case readHost str of
-      Nothing -> fail $ "Could not parse host address: " <> str
-      Just host -> pure host
-
-  -- Like cardano-cli: "mainnet" or a number for a given testnet network magic.
-  parseNetwork str =
-    case parseMainnet str <|> parseTestnetMagic str of
-      Nothing -> fail $ "Could not parse network id: " <> str <> " (Expected 'mainnet' or a number)"
-      Just nid -> pure nid
-
-  parseMainnet str = Mainnet <$ guard (str == "mainnet")
-
-  parseTestnetMagic = fmap (Testnet . NetworkMagic) . readMaybe
-
--- | Like 'lookupEnv' but terminate program with a message if environment
--- variable is not set.
-requireEnv :: String -> IO String
-requireEnv name =
-  lookupEnv name >>= \case
-    Just value -> pure value
-    Nothing -> die $ "Error: Required environment variable " <> name <> " not set"
-
-websocketApp :: Host -> WS.PendingConnection -> IO ()
-websocketApp host pendingConnection = do
-  frontend <- WS.acceptRequest pendingConnection
-  withClient host $ \backend ->
-    concurrently_
-      (forever $ WS.receive frontend >>= WS.send backend)
-      (forever $ WS.receive backend >>= WS.send frontend)
-
-httpApp :: NetworkId -> FilePath -> WS.Connection -> Application
-httpApp networkId key cnx req send =
+httpApp :: Application
+httpApp req send =
   case (requestMethod req, pathInfo req) of
-    ("GET", "paint" : args) -> do
-      case traverse (readMay . toString) args of
-        Just [x, y, red, green, blue] -> do
-          putStrLn $ show (x, y) <> " -> " <> show (red, green, blue)
-          paintPixel networkId key cnx Pixel{x, y, red, green, blue}
-          send $ responseLBS status200 corsHeaders "OK"
-        _ ->
-          send handleError
     ("HEAD", _) -> send $ responseLBS status200 corsHeaders ""
     -- Statically serve files
     ("GET", []) -> send $ handleFile "index.html"
@@ -91,8 +41,6 @@ httpApp networkId key cnx req send =
     _ ->
       send handleNotFound
  where
-  handleError = responseLBS status400 corsHeaders "INVALID REQUEST"
-
   handleNotFound = responseLBS status404 corsHeaders "NOT FOUND"
 
   handleFile filepath = responseFile status200 corsHeaders filepath Nothing
--- hydraw/static/bundle.js
+++ hydraw/static/bundle.js
@@ -1,33 +1,3 @@
-const protocol = window.location.protocol == "https:" ? "wss:" : "ws:";
-const client = new WebSocket(protocol + "//" + window.location.host);
-
-const metadataLabel = 14;
-const query = (window.location.search || "?")
-  .substr(1)
-  .split('&')
-  .filter(x => x != '')
-  .map(x => x.split('='))
-const delay = Number((query.filter(([k, v]) => k == 'delay')[0] || [])[1] || 0);
-
-let n = 0
-client.addEventListener("message", e => {
-  const msg = JSON.parse(e.data);
-  switch (msg.tag) {
-    case "TxValid":
-      // TODO: Should only draw pixels on SnapshotConfirmed
-      console.log("New transaction seen", msg.transaction.id);
-      if (msg.transaction.auxiliaryData != null) {
-        console.log("Transaction has auxiliary data", msg.transaction.auxiliaryData);
-        const aux = cbor.decodeFirstSync(msg.transaction.auxiliaryData).value;
-        const [x, y, r, g, b] = (aux.get(0) || aux.get(1)).get(metadataLabel);
-        n += delay;
-        setTimeout(() => drawPixel(x, y, [r, g, b]), n);
-      }
-    default:
-      console.log("Irrelevant message", msg);
-  }
-});
-
 // Canvas
 
 const CANVAS_SIZE = 32;
@@ -35,20 +5,19 @@ const CANVAS_SIZE = 32;
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const canvasScale = {
-  x: canvas.width/CANVAS_SIZE,
-  y: canvas.height/CANVAS_SIZE,
+  x: canvas.width / CANVAS_SIZE,
+  y: canvas.height / CANVAS_SIZE,
 }
 ctx.scale(canvasScale.x, canvasScale.y);
 console.log("canvasScale", canvasScale);
 
 const drawPixel = (x, y, rgb) => {
-  const [r,g,b] = rgb;
+  const [r, g, b] = rgb;
   ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
   ctx.fillRect(x, y, 1, 1);
 }
 
-
-canvas.addEventListener('click', function(e) {
+canvas.addEventListener('click', function (e) {
   console.log("event", e);
   const canvasPosition = {
     x: canvas.offsetLeft,
@@ -64,21 +33,18 @@ canvas.addEventListener('click', function(e) {
   const x = Math.floor(clickedPixel.x);
   const y = Math.floor(clickedPixel.y);
 
-  const [r, g, b] = currentColor;
-  fetch(`/paint/${x}/${y}/${r}/${g}/${b}`)
-    .then(() => console.log("Ok"))
-    .catch(e => console.log("Error", e));
+  drawPixel(x, y, currentColor);
 });
 
 // Color picker
 
-let currentColor = [255,0,0];
+let currentColor = [255, 0, 0];
 const currentColorElement = document.querySelector('#current-color');
 const picker = new Picker(currentColorElement);
 
 currentColorElement.style.background = `rgb(${currentColor[0]}, ${currentColor[1]}, ${currentColor[2]})`;
 
-picker.onDone = function(color) {
+picker.onDone = function (color) {
   console.log("Color picked:", color);
   currentColor = color.rgba;
   currentColorElement.style.background = color.rgbaString;
